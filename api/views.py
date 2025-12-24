eebf2be68171679bb1bb8e7a899253f2a996c548
vishalipar <vishalipar04@gmail.com>
2025-12-24 14:59:24 +0530

    Delete the employee using class based view

--- api/views.py
+++ api/views.py
@@ -79,3 +79,8 @@ class EmployeeDetail(APIView):
             serializer.save()
             return Response(serializer.data, status = status.HTTP_200_OK)
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+        
+    def delete(self, request, pk):
+        employee = self.get_object(pk)
+        employee.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
