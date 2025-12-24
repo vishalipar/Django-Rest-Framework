c8bef9935f0261cf3b434949a251a4c4b204e28a
vishalipar <vishalipar04@gmail.com>
2025-12-24 14:18:55 +0530

    Post the data through class based view

--- api/views.py
+++ api/views.py
@@ -50,3 +50,10 @@ class Employees(APIView):
         employees = Employee.objects.all()
         serializer = EmployeeSerializer(employees, many=True)
         return Response(serializer.data, status = status.HTTP_200_OK)
+        
+    def post(self, request):
+        serializer = EmployeeSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        return Respnose(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
