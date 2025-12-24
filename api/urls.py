1be69d227cac4dbf51f2b02cee5dd17982fde002
vishalipar <vishalipar04@gmail.com>
2025-12-24 14:45:27 +0530

    Get a particular employee detail

--- api/urls.py
+++ api/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('student/<int:pk>/', views.studentDetailView, name='studentDetailView'),
     
     path('employees/', views.Employees.as_view() , name='Employees'),
+    path('employees/<int:pk>/', views.EmployeeDetail.as_view() , name='EmployeeDetail'),
 ]
--- api/views.py
+++ api/views.py
@@ -5,6 +5,7 @@ from .serializers import StudentSerializer, EmployeeSerializer
 from students.models import Student
 from employees.models import Employee
 from rest_framework.views import APIView
+from django.http import Http404
 
 
 @api_view(['GET', 'POST'])
@@ -57,3 +58,16 @@ class Employees(APIView):
             serializer.save()
             return Response(serializer.data, status = status.HTTP_201_CREATED)
         return Respnose(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+        
+        
+class EmployeeDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Employee.objects.get(pk=pk)
+        except Employee.DoesNotExist:
+            raise Http404
+            
+    def get(self, request, pk):
+        employee = self.get_object(pk)
+        serializer = EmployeeSerializer(employee)
+        return Response(serializer.data, status = status.HTTP_200_OK)
