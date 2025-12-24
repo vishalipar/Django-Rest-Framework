c8499845c88c0e1eb4db98cc58e409502f2c165e
vishalipar <vishalipar04@gmail.com>
2025-12-24 13:31:43 +0530

    View particular student through id

--- api/views.py
+++ api/views.py
@@ -19,3 +19,15 @@ def studentsView(request):
             return Response(serializer.data, status = status.HTTP_201_CREATED)
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
         
+        
+@api_view(['GET'])
+def studentDetailView(request, pk):
+    try:
+        student = Student.objects.get(pk=pk)
+    except:
+        return Response(status = status.HTTP_404_NOT_FOUND)
+        
+    if request.method == 'GET':
+        serializer = StudentSerializer(student)
+        return Response(serializer.data, status = status.HTTP_200_OK)
+        
